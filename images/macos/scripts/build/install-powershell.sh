3fe17653af6cbee4ac7b1befc52c0fafb948d063
David Weese <david.weese@data4life.care>
2024-05-16 10:43:41 -0400

    fix permissions

--- images/macos/scripts/build/install-powershell.sh
+++ images/macos/scripts/build/install-powershell.sh
@@ -55,7 +55,7 @@ done
 
 # Fix permission root => runner after installing powershell for arm64 arch
 if [[ $arch == "arm64" ]]; then
-    sudo chown -R $USER ~/.local ~/.cache ~/.config
+    sudo chown -R $USER $HOME/.local $HOME/.cache $HOME/.config
 fi
 
 # A dummy call to initialize .IdentityService directory
